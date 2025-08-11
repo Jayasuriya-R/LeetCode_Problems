a0559093407fda35c59cb8a666e6df5ebe0ec410
Jayasuriya-R <suryaabi3801@gmail.com>
2025-08-11 17:40:56 +0530

    reverse_linkedlist.js

---
+++ GeeksForGeeks/Linked List/problems/reverse_linkedlist.js
@@ -0,0 +1,12 @@
+var reverseList = function(head) {
+    let prev = null
+    let cur = head
+
+    while(cur != null){
+        let next = cur.next
+        cur.next = prev
+        prev = cur
+        cur = next
+    }
+    return prev
+};

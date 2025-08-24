a6ac24e3db5625be80132fe3f41f1744da46ad18
Jayasuriya-R <suryaabi3801@gmail.com>
2025-08-24 19:39:36 +0530

    stack_linkedlist.js

---
+++ GeeksForGeeks/Stack/problems/stack_linkedlist.js
@@ -0,0 +1,35 @@
+class Node{
+    constructor(x){
+        this.data = x
+        this.next = null
+    }
+}
+class stack{
+    constructor(){
+        this.head=null
+        this.size=0
+    }
+    size(){
+        return this.size
+    }
+    isEmpty(){
+        return this.head === null
+    }
+    push(x){
+        let temp = new Node(x);
+        temp.next = this.head
+        this.head = temp;
+        this.size +=1
+    }
+    pop(){
+        if(this.head == null)return null
+        let res = this.head.data
+        this.head = this.head.next
+        this.size -=1
+        return res
+    }
+    peek(){
+        if(this.head == null)return null
+        return this.head.data;
+    }
+}
